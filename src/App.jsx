5f047924a02702f8acc39b971c62773ca63450aa
acki <ackimixs@protonmail.com>
2026-03-06 15:42:47 +0100

    default mode

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,31 @@ function App() {
     document.body.classList.toggle("dark", mq.matches);
   }, []);
 
-  const fetchWeather = async (lat, long) => {
+  const save = (data) => {
+    localStorage.setItem('weatherData', JSON.stringify(data));
+  }
+
+  const fetchWeather = async (lat, long, fallback = false) => {
     setLoading(true);
 
     try {
+      console.error('test');
       const data = await fetchWeatherData(lat, long);
       setWeatherData(data);
       setLoading(false);
+      save(data);
       console.log(data);
     } catch (error) {
       console.error('Error fetching weather data:', error);
+      if (fallback) {
+        const weather = localStorage.getItem('weatherData');
+        if (weather) {
+          setLoading(false);
+          setWeatherData(JSON.parse(weather));
+          return;
+        }
+      }
+      setLoading(false);
       setError({ hasError: true, message: 'Impossible de récupérer les données météo. Veuillez réessayer plus tard.' });
     }
   }
@@ -33,12 +48,19 @@ function App() {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
-      fetchWeather(latitude, longitude);
+      console.log("test");
+      fetchWeather(latitude, longitude, true);
     }, (error) => {
       console.error('Error getting geolocation:', error);
+      const weather = localStorage.getItem('weatherData');
+      if (weather) {
+        setLoading(false);
+        setWeatherData(JSON.parse(weather));
+        return;
+      }
       setError({ hasError: true, message: 'Impossible de récupérer votre position. Veuillez autoriser l\'accès à la géolocalisation.' });
       setLoading(false);
-    });
+    }, {timeout: 1000});
   }, []);
 
   const getAdvice = (weather) => {
@@ -96,27 +118,27 @@ function App() {
                   <span>{err.message}</span>
                 </div>
             :
-                <>
-              <div className="location">
-                <span className="city">{weatherData.name.toUpperCase()}</span> · <span
-                  className="country">{weatherData.sys.country.toUpperCase()}</span>
-              </div>
-              <hr />
-
-              <div className="temperature-section">
-                <div>
-                  <div className="temperature">{weatherData.main.temp}°C</div>
-                  <div className="weather-description">{weatherData.weather[0].description}</div>
+              <>
+                <div className="location">
+                  <span className="city">{weatherData.name.toUpperCase()}</span> · <span
+                    className="country">{weatherData.sys.country.toUpperCase()}</span>
                 </div>
-                <img
-                    src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}.png`}
-                    alt="Weather Icon"
-                    className="weather-icon"
-                />
-              </div>
-              <hr />
-              <div className="advice">{getAdvice(weatherData.weather[0].main)}</div>
-            </>
+                <hr />
+
+                <div className="temperature-section">
+                  <div>
+                    <div className="temperature">{weatherData.main.temp}°C</div>
+                    <div className="weather-description">{weatherData.weather[0].description}</div>
+                  </div>
+                  <img
+                      src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}.png`}
+                      alt="Weather Icon"
+                      className="weather-icon"
+                  />
+                </div>
+                <hr />
+                <div className="advice">{getAdvice(weatherData.weather[0].main)}</div>
+              </>
       }
     </div>
   )
