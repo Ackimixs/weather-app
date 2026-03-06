81bb2af66aa8ea0ea5b8aafb4b282f52b1f26e2f
acki <ackimixs@protonmail.com>
2026-03-06 17:08:08 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -76,69 +76,71 @@ function App() {
   }
 
   return (
-    <div className="weather-card">
-      <div className="search-bar">
-        <div className="search-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-               stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
-          </svg>
-        </div>
-        <input
-            type="text"
-            placeholder="Rechercher"
-            className="search-input"
-            onKeyDown={async (e) => {
-              if (e.key === 'Enter') {
-                try {
-                  const { lat, lon } = await convertCityToCoordinates(e.target.value);
-                  await fetchWeather(lat, lon);
-                } catch (error) {
-                  console.error('Error fetching weather data:', error);
-                  setError({
-                    hasError: true,
-                    message: 'Impossible de récupérer les données météo pour cette ville. Veuillez réessayer.'
-                  });
-                }
-              }
-            }}
-        />
-      </div>
-
-      {
-        loading ?
-            <div className="loading">
-              <span>Chargement ...</span>
+      <div className="app">
+        <div className="weather-card">
+          <div className="search-bar">
+            <div className="search-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                   stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round"
+                      d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
+              </svg>
             </div>
-            : err.hasError ?
-                <div className="loading">
-                  <span>{err.message}</span>
-                </div>
-            :
-              <>
-                <div className="location">
-                  <span className="city">{weatherData.name.toUpperCase()}</span> · <span
-                    className="country">{weatherData.sys.country.toUpperCase()}</span>
-                </div>
-                <hr />
+            <input
+                type="text"
+                placeholder="Rechercher"
+                className="search-input"
+                onKeyDown={async (e) => {
+                  if (e.key === 'Enter') {
+                    try {
+                      const { lat, lon } = await convertCityToCoordinates(e.target.value);
+                      await fetchWeather(lat, lon);
+                    } catch (error) {
+                      console.error('Error fetching weather data:', error);
+                      setError({
+                        hasError: true,
+                        message: 'Impossible de récupérer les données météo pour cette ville. Veuillez réessayer.'
+                      });
+                    }
+                  }
+                }}
+            />
+          </div>
 
-                <div className="temperature-section">
-                  <div>
-                    <div className="temperature">{weatherData.main.temp}°C</div>
-                    <div className="weather-description">{weatherData.weather[0].description}</div>
-                  </div>
-                  <img
-                      src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}.png`}
-                      alt="Weather Icon"
-                      className="weather-icon"
-                  />
+          {
+            loading ?
+                <div className="loading">
+                  <span>Chargement ...</span>
                 </div>
-                <hr />
-                <div className="advice">{getAdvice(weatherData.weather[0].main)}</div>
-              </>
-      }
-    </div>
+                : err.hasError ?
+                    <div className="error">
+                      <span>{err.message}</span>
+                    </div>
+                    :
+                    <>
+                      <div className="location">
+                        <span className="city">{weatherData.name.toUpperCase()}</span> · <span
+                          className="country">{weatherData.sys.country.toUpperCase()}</span>
+                      </div>
+                      <hr />
+
+                      <div className="temperature-section">
+                        <div>
+                          <div className="temperature">{weatherData.main.temp}°C</div>
+                          <div className="weather-description">{weatherData.weather[0].description}</div>
+                        </div>
+                        <img
+                            src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}.png`}
+                            alt="Weather Icon"
+                            className="weather-icon"
+                        />
+                      </div>
+                      <hr />
+                      <div className="advice">{getAdvice(weatherData.weather[0].main)}</div>
+                    </>
+          }
+        </div>
+      </div>
   )
 }
 
